79e849442ebf1820c275320144bfa12a62c91d37
Muzingaye <muzingaye.dube@gmail.com>
2026-06-22 15:41:51 +0200

    products_backend

---
+++ server/drf/api/serialzers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from  .models import Product, Order, OrderItem
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model  = Product
+        fields = (
+            'name',
+            'description',
+            'price',
+            'stock',
+        )
+        
+
+    def validate_price(self, val):
+        if val <= 0:
+            raise serializers.ValidationError(
+                "Price must be greater than 0"
+            )
+        return val
+        
+    def create(self, validated_data):
+        return Product(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.content = validated_data.get('content', instance.content)
+        instance.created = validated_data.get('created', instance.created)
+        return instance
