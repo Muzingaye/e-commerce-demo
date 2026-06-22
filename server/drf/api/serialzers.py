91ad24656e248685f7a98dbcd1adb69baa476294
Muzingaye <muzingaye.dube@gmail.com>
2026-06-22 21:52:33 +0200

    serializers

--- server/drf/api/serialzers.py
+++ server/drf/api/serialzers.py
@@ -29,3 +29,29 @@ class ProductSerializer(serializers.ModelSerializer):
         instance.content = validated_data.get('content', instance.content)
         instance.created = validated_data.get('created', instance.created)
         return instance
+    
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = (
+            'product',
+            'quantity',
+        )
+        
+class OrderSerializer(serializers.ModelSerializer):
+    items =  OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = (
+            'order_id',
+            'user',
+            'created_at',
+            'status',
+            'items',
+        )
+
+
+
