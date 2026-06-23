e0a72be1593f1b7aa6a37ea6a2567fb2dc5fcd3e
Muzingaye <muzingaye.dube@gmail.com>
2026-06-23 19:48:05 +0200

    orders

--- server/drf/api/serialzers.py
+++ server/drf/api/serialzers.py
@@ -33,15 +33,28 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    
+    product_name = serializers.CharField(source = 'product.name')
+    product_price = serializers.DecimalField(max_digits=10, decimal_places=2,
+                                          source = 'product.price',
+                                          )
     class Meta:
         model = OrderItem
         fields = (
-            'product',
+            'product_name',
+            'product_price',
             'quantity',
+            'item_subtotal'
         )
         
 class OrderSerializer(serializers.ModelSerializer):
     items =  OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+
+    def get_total_price(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
 
     class Meta:
         model = Order
@@ -51,6 +64,7 @@ class OrderSerializer(serializers.ModelSerializer):
             'created_at',
             'status',
             'items',
+            'total_price',
         )
 
 
